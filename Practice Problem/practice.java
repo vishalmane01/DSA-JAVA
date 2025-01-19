5addda4b8d406621151dfc7c06ad2f9ea20cceee
vishalmane01 <vishal012mane@gmail.com>
2025-01-19 23:49:15 +0530

    Day 2.2

--- Practice Problem/practice.java
+++ Practice Problem/practice.java
@@ -1,14 +1,2 @@
-import java.util.Scanner;
-public class practice{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter your name:");
-        String name = scanner.nextLine();
-        
-        System.out.print("Enter your age:");
-        int age = scanner.nextInt();
-
-        System.out.println("Name: "+ name + ", Age: "+age);
-    }
-}
+// To find out whether the given String is Palindrome or not.
+// To find Armstrong Number between two given number.
