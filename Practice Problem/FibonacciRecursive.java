ebb584d2ce2d0906718ece7abb6f0d58876675f6
vishalmane01 <vishal012mane@gmail.com>
2025-01-21 19:30:30 +0530

    SumAnd Product

--- Practice Problem/FibonacciRecursive.java
+++ Practice Problem/FibonacciRecursive.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
-public class FibonacciRecursive {
-    public static void main(String[] args) {
-        Scanner sc =  new Scanner(System.in);
+public class FibonacciRecursive{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of terms: ");
         int n = sc.nextInt();
 
-        System.out.println("Fibonacci Series (Recursive):");
-        for (int i = 0; i < n; i++) {
+        System.out.println("Fibonacci Series (Recursive): ");
+        for(int i=0 ; i<=n ; i++){
             System.out.print(fibonacci(i) + " ");
         }
     }
 
-    public static int fibonacci(int n) {
-        if (n <= 1) {
+    public static int fibonacci(int n){
+        if(n<=1){
             return n;
+        }else{
+            return fibonacci(n-2) + fibonacci(n-1);
         }
-        return fibonacci(n - 1) + fibonacci(n - 2);
     }
+    
 }
---
+++ Practice Problem/SumAndProduct.java
@@ -0,0 +1,25 @@
+// input: n = 234
+// Output: 15 
+// Explanation: 
+// Product of digits = 2 * 3 * 4 = 24 
+// Sum of digits = 2 + 3 + 4 = 9 
+// Result = 24 - 9 = 15
+public class SumAndProduct {
+    public static void main(String[] args){
+        int n = 235;
+        int result = CalculateDifference(n);
+        System.out.print(" " + result);
+    }  
+  
+    public static int CalculateDifference(int n){
+        int sum = 0;
+        int product = 1;
+        while (n>0){
+            int digit = n%10;
+            sum += digit;
+            product *= digit;
+            n /= 10;
+        }
+        return product - sum;
+    }
+}
